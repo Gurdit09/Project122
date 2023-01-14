0bc896c9c84500758924101ffe01ad03c203b488
Gurdit09 <90044541+Gurdit09@users.noreply.github.com>
2023-01-14 09:42:19 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -50,16 +50,19 @@ function setup() {
 function draw() {
   if(draw_apple == "set")
   {
-    for(var i = 1;i <= to_number;i++)
-    x = Math.floor(Math.random()*700);
-    y = Math.floor(Math.random()*400);
-    image(apple,x,y,50,50) }
+    for(var i = 1 ; i <= to_number; i++)
+    {
+      x = Math.floor(Math.random() * 700);
+      y = Math.floor(Math.random() * 400);
+      image(apple, x, y, 50, 50);
+    }
     document.getElementById("status").innerHTML = to_number + " Apples drawn";
     speak_data = to_number + "Apples drawn";
-    speak()
+    speak();
     draw_apple = "";
-  
+  }
 }
+
 
 function speak(){
     var synth = window.speechSynthesis;
